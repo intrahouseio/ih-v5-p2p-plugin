07cca79a77e567adf308b9353cee576361e3d44c
Artem Ivanov <fast0490f@mail.ru>
2021-05-04 15:53:16 +0300

    init

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const hwid = '13582c8df904a9beaa3635b48333ed5ae2e07b7624f69e65b87dfc1f94078d7d-0
 function main(options) {
   console.log('ih-v5-p2p-plugin')
 
-  const p2p = p2pClient({ hwid });
-  const notify = notifyClient({ hwid });
+  const p2p = p2pClient({ hwid, path: '/opt/ih-v5' });
+  const notify = notifyClient({ hwid, path: '/opt/ih-v5' });
 
   p2p.on('debug', (mes) => console.log(mes))
 
