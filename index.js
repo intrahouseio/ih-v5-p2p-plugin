7ad84b54916880cba045080952b3bdf3c1de9806
Artem Ivanov <fast0490f@mail.ru>
2021-05-13 17:37:05 +0300

    version 5.0.0

--- index.js
+++ index.js
@@ -1,25 +1,49 @@
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
+const path = require('path');
+
+const logger = require('./lib/logger');
 const notifyClient = require('./lib/notify');
 const p2pClient = require('./lib/p2p');
 
 
-const hwid = '13582c8df904a9beaa3635b48333ed5ae2e07b7624f69e65b87dfc1f94078d7d-0110'
+function sendProcessInfo() {
+  const mu = process.memoryUsage();
+  const memrss = Math.floor(mu.rss / 1024);
+  const memheap = Math.floor(mu.heapTotal / 1024);
+  const memhuse = Math.floor(mu.heapUsed / 1024);
 
+  const data = { memrss, memheap, memhuse };
 
-function main(options) {
-  console.log('ih-v5-p2p-plugin')
-
-  const p2p = p2pClient({ hwid, port: 8088, path: '/opt/ih-v5' });
-  const notify = notifyClient({ hwid, port: 8088, path: '/opt/ih-v5' });
-
-  p2p.on('debug', (mes) => console.log(mes))
-
-  notify.on('state', (info) => console.log(info))
-  notify.on('debug', (mes) => console.log(mes))
-  notify.on('session', (sessionid) => p2p.emit('start', sessionid))
+  process.send({ type: 'procinfo', data });
 }
 
+function main(options) {
+  let opt;
+  try {
+    opt = JSON.parse(process.argv[2]);
+  } catch (e) {
+    opt = {};
+  }
 
+  const logfile = opt.logfile || path.join(__dirname, 'ih_p2p.log');
+  const loglevel = opt.loglevel || 0;
+
+  logger.start(logfile, loglevel);
+  logger.log('Plugin xp2p has started  with args: ' + process.argv[2]);
+
+  setInterval(sendProcessInfo, 10000);
+
+  const p2p = p2pClient(opt);
+  const notify = notifyClient(opt);
+
+  p2p.on('debug', mes => logger.log(mes))
+
+  notify.on('state', data => process.send({ type: 'procinfo', data }))
+  notify.on('debug', mes => logger.log(mes))
+  notify.on('session', sessionid => p2p.emit('start', sessionid))
+
+  sendProcessInfo();
+}
 
 main();
