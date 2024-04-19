ad3919a1cc85e9f44b8670fe3be30989ff55c76a
Artem Ivanov <fast0490f@mail.ru>
2024-04-19 17:42:04 +0300

    release v5.2.0

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const { fork } = require('child_process');
 
 
 const params = {
-  port :8088,
+  port :9999,
   syspath: '/opt/ih-v5',
   hwid: '23a2cab6b81b02d18668fa676e8f3c4eb68577cb33f02be50774b4bfa742ae09-1110',
   logfile: '/opt/ih-v5/log/ih_p2p.log',
